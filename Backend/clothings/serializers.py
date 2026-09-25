79a526cdcc582785ac51a59441079941e3152c88
Hanz Rowie <hanzrowiegurung@gmail.com>
2026-09-25 20:12:52 +0545

    Refactor ClothingItemSerializer to improve readability and structure

--- Backend/clothings/serializers.py
+++ Backend/clothings/serializers.py
@@ -2,22 +2,23 @@ from rest_framework import serializers
 from clothings.models import ClothingItem
 
 class ClothingItemSerializer(serializers.ModelSerializer):
-    model = ClothingItem
+    class Meta:
+        model = ClothingItem
 
-    fields = [
-        "id",
-        "name",
-        "category",
-        "image",
-        "color",
-        "style",
-        "occasion",
-        "is_active",
-        "created_at"
-    ]
+        fields = [
+            "id",
+            "name",
+            "category",
+            "image",
+            "color",
+            "style",
+            "occasion",
+            "is_active",
+            "created_at",
+        ]
 
-    read_only_fields = [
-        "id",
-        "created_at",
-    ]
+        read_only_fields = [
+            "id",
+            "created_at",
+        ]
     
