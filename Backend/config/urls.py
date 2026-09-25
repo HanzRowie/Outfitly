950e3e3f6bd16075c05ef4b2eee2253cdf70c1cd
Hanz Rowie <hanzrowiegurung@gmail.com>
2026-09-25 20:08:56 +0545

    Add outfits app with initial configuration and admin registration

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -4,9 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        "api/accounts/",
-        include("accounts.urls")
-    ),
+    path("api/accounts/",include("accounts.urls")),
+    path("api/clothing/", include("clothings.urls")),
+    path("api/outfits/",include("outfits.urls")),
 ]
 
